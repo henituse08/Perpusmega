d52a2f6e58ad2f790419a236dbd7fc308ead0bf7
henituse08 <megaananda6955@gmail.com>
2022-05-18 08:15:39 +0700

    Halaman Home

---
+++ Home Page/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan mega</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-light" style="background-color: #B2C3FF;" id ="navbar">
+  <div class="container-fluid">
+    <div class="logo">
+    <img src="../Asset/Frame.png" alt="book">
+    <a class="navbar-brand" id="logo" href="#">Sistem Perpustakaan Taman Baca Palembang</a>
+    </div>
+    <div class="d-flex justify-content-start-end">
+    <ul class="navbar-nav flex-row">
+        <li class="nav-item p-2" >
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item p-2">
+          <a class="nav-link" href="https://www.facebook.com/">Registrasi</a>
+        </li>
+        <li class="nav-item p-2">
+          <a class="nav-link" href="https://www.facebook.com/">Login Admin</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+          <div class="p-5 container">
+            <h5 class="card-title">Selamat Datang di Perpustakaan Mega</h5>
+            <p class="card-text">Sistem Perpustakaan Taman Baca Palembang</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
